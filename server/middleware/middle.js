94326d4c3778b3a0cbf57e86c5d736d1ff5ab0ec
MatasRu <matas.rudaminas@gmail.com>
2021-05-20 12:40:01 +0300

    Updated Middleware

--- server/middleware/middle.js
+++ server/middleware/middle.js
@@ -7,7 +7,12 @@ async function middleware (req, res, next){
     if (req.body.name.length < 3) {
         return errorMessage("User Name is too short, it should be more then 3 characters")
     }
-
+    if (!Number(req.body.age)) {
+        return errorMessage("User Age must be a number")
+    }
+    if (!req.body.email.includes("@") && !req.body.email.includes(".")) {
+        return errorMessage("Email address should have @ and . symbols")
+    }
 
     return next()
 }
